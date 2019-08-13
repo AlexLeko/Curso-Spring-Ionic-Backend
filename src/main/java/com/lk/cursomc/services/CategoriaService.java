6fbf1b7b34ad5839aebb034e1f2acad7e3c0af01
AlexLeko <lk.alexds@gmail.com>
2019-08-13 20:37:31 -0300

    Tratamento de Exception

--- src/main/java/com/lk/cursomc/services/CategoriaService.java
+++ src/main/java/com/lk/cursomc/services/CategoriaService.java
@@ -2,6 +2,7 @@ package com.lk.cursomc.services;
 
 import com.lk.cursomc.domain.Categoria;
 import com.lk.cursomc.repositories.CategoriaRepository;
+import com.lk.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +16,8 @@ public class CategoriaService {
 
     public Categoria buscar(Integer id){
         Optional<Categoria> categoria = _repository.findById(id);
-        return categoria.orElse(null);
+        return categoria.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! ID: " + id + " , TIPO: " + Categoria.class.getName()));
     }
 
 
