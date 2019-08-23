ffa8ab75b87f6597ca4ad60d1e16b21df42bd987
Alex dos Santos <s2it_alsantos@uolinc.com>
2019-08-23 17:44:10 -0300

    Lista de Categorias com DTO

---
+++ src/main/java/com/lk/cursomc/dto/CategoriaDTO.java
@@ -0,0 +1,48 @@
+package com.lk.cursomc.dto;
+
+import com.lk.cursomc.domain.Categoria;
+
+import java.io.Serializable;
+
+public class CategoriaDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private String nome;
+
+    // ===================
+    //     CONSTRUCTORS
+    // ===================
+
+    public CategoriaDTO() {
+    }
+
+    public CategoriaDTO(Categoria obj) {
+        this.id = obj.getId();
+        this.nome = obj.getNome();
+    }
+
+    // ===================
+    // GETTERS AND SETTERS
+    // ===================
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+
+
+}
--- src/main/java/com/lk/cursomc/resource/CategoriaResource.java
+++ src/main/java/com/lk/cursomc/resource/CategoriaResource.java
@@ -1,6 +1,7 @@
 package com.lk.cursomc.resource;
 
 import com.lk.cursomc.domain.Categoria;
+import com.lk.cursomc.dto.CategoriaDTO;
 import com.lk.cursomc.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -51,6 +53,14 @@ public class CategoriaResource {
         _service.delete(id);
 
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(method=RequestMethod.GET)
+    public ResponseEntity<List<CategoriaDTO>> findAll() {
+        List<Categoria> list = _service.findAll();
+        List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+
+        return ResponseEntity.ok().body(listDTO); // status: 200 OK
     }
 
 
