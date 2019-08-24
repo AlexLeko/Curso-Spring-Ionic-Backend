2451ff5761a75b42e9042343c4c0cb721b94a3e9
Alex dos Santos <s2it_alsantos@uolinc.com>
2019-08-24 19:45:03 -0300

    Retorno de Validação Personalizado

--- src/main/java/com/lk/cursomc/resource/exception/ResourceExceptionHandler.java
+++ src/main/java/com/lk/cursomc/resource/exception/ResourceExceptionHandler.java
@@ -4,14 +4,16 @@ import com.lk.cursomc.services.exceptions.DataIntegrityException;
 import com.lk.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
 import javax.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
+    // Tratativa para objeto não encontado.
     @ExceptionHandler(ObjectNotFoundException.class)
     public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException exc, HttpServletRequest request){
         StandardError err = new StandardError(
@@ -22,6 +24,7 @@ public class ResourceExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);   // status: 404
     }
 
+    // tratativa quando tentar excluir uma categoria que possui produtos
     @ExceptionHandler(DataIntegrityException.class)
     public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException exc, HttpServletRequest request){
         StandardError err = new StandardError(
@@ -32,6 +35,20 @@ public class ResourceExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err); // status: 400
     }
 
+    // Tratativa para mensagem de validação de parametros no endpoint
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> validation(MethodArgumentNotValidException exc, HttpServletRequest request){
+        ValidationError err = new ValidationError(
+                HttpStatus.BAD_REQUEST.value(),
+                "Erro de validação!",
+                System.currentTimeMillis());
 
+        // recupera os erros da exception e add a nossa tratativa de validação.
+        for (FieldError fe : exc.getBindingResult().getFieldErrors()){
+            err.addError(fe.getField(), fe.getDefaultMessage());
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err); // status: 400
+    }
 
 }
