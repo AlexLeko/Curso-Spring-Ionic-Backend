837e7d84352289b4f5c42c722cb947849f5d1111
Alex dos Santos <s2it_alsantos@uolinc.com>
2019-08-25 19:01:30 -0300

    Validação Customizada | E-mail já cadastrado

--- src/main/java/com/lk/cursomc/domain/Cliente.java
+++ src/main/java/com/lk/cursomc/domain/Cliente.java
@@ -14,9 +14,14 @@ public class Cliente implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String nome;
+
+    @Column(unique = true)
     private String email;
+
     private String cpfCnpj;
+
     private Integer tipo;
 
 
--- src/main/java/com/lk/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/lk/cursomc/services/validation/ClienteInsertValidator.java
@@ -1,9 +1,12 @@
 package com.lk.cursomc.services.validation;
 
+import com.lk.cursomc.domain.Cliente;
 import com.lk.cursomc.domain.enums.TipoCliente;
 import com.lk.cursomc.dto.ClienteNewDTO;
+import com.lk.cursomc.repositories.ClienteRepository;
 import com.lk.cursomc.resource.exception.FieldMessage;
 import com.lk.cursomc.services.validation.utils.BR;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
@@ -11,6 +14,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+    @Autowired
+    private ClienteRepository _clienteRepository;
+
 
     @Override
     public void initialize(ClienteInsert ann){
@@ -21,16 +28,26 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 
         List<FieldMessage> list = new ArrayList<>();
 
-        // inclua os testes aqui, inserindo erros na lista;
+        // === VALIDAÇÕES ===
 
+        // Valida CPF valido.
         if (dto.getTipo().equals(TipoCliente.PESSOA_FISICA.getCodigo()) && !BR.isValidCPF(dto.getCpfCnpj())){
             list.add(new FieldMessage("cpfCnpj", "CPF Inválido!"));
         }
 
+        // Valida CNPJ Valido.
         if (dto.getTipo().equals(TipoCliente.PESSOA_JURIDICA.getCodigo()) && !BR.isValidCNPJ(dto.getCpfCnpj())){
             list.add(new FieldMessage("cpfCnpj", "CNPJ Inválido!"));
         }
 
+        // Valida E-mail já existente.
+        Cliente cli = _clienteRepository.findByEmail(dto.getEmail());
+        if (cli != null){
+            list.add(new FieldMessage("email", "E-mail já cadastrado."));
+        }
+
+
+        // inclua os testes aqui, inserindo erros na lista;
         for (FieldMessage e : list){
             context.disableDefaultConstraintViolation();
             context.buildConstraintViolationWithTemplate(e.getMessage())
