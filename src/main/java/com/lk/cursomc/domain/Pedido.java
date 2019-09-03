550c0d858f44ce035d6921097f08770958b0d747
Alex dos Santos <s2it_alsantos@uolinc.com>
2019-09-03 17:35:09 -0300

    Total e Sub-Total do Pedido

--- src/main/java/com/lk/cursomc/domain/Pedido.java
+++ src/main/java/com/lk/cursomc/domain/Pedido.java
@@ -49,6 +49,14 @@ public class Pedido implements Serializable {
         this.enderecoEntrega = enderecoEntrega;
     }
 
+    public double getValorTotal(){
+        double soma = 0.0;
+        for (ItemPedido item : itens){
+            soma = soma + item.getSubTotal();
+        }
+        return soma;
+    }
+
 
     // ===================
     // GETTERS AND SETTERS
